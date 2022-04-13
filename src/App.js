30d438473f9b583ca6b36a43cf6c7118132e04f6
rabbit <rabbit@!.com>
2022-04-14 05:57:14 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
 
 import Home from './components/Views/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+       <Router>
+         <Routes>
+           <Route exact path="/" element={<Home />} />
+           <Route exact path="/about"  element={<About />} />
+         </Routes>
+       </Router>
     </div>
   );
 }
--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -1,55 +1,54 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import './Home.css';
 
 export default function Home() {
+    const navigate = useNavigate();
+
+    const about = () => {
+        navigate('/about');
+    }
     return (
         <div className="home">
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-10">
-                        <div>
-                            <p className="d-flex nft-text1">KISS ME NFTs + Token</p>
-                            <p className="d-flex nft-text2">SALE, LOTTERY & CHARITY</p>
-                        </div>
-                        <div className="container" style={{marginTop: "25%"}}>
-                            <div className="row">
-                                <div className="col-sm-4">
-                                    <div className="d-flex justify-content-start">
-                                        <div className="carousel slide" data-bs-ride="carousel">
-                                            <div className="carousel-inner" style={{width:"100%"}}>
-                                                <div className="carousel-item active">
-                                                    <img src="img/lips.png" className="d-block" />
-                                                </div>
-                                                <div className="carousel-item">
-                                                    <img src="img/lips.png"  className="d-block" />
-                                                </div>
-                                                <div className="carousel-item">
-                                                    <img src="img/lips.png"  className="d-block" />
-                                                </div>
-                                            </div>
-                                        </div>
+                    <div>
+                        <span className="d-flex nft-text1">KISS ME NFTs + Token</span>
+                        <span className="d-flex nft-text2">SALE, LOTTERY & CHARITY</span>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-3 main-container">
+                        <div className="d-flex justify-content-center">
+                            <div className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <img src="img/lips.png" className="d-block" />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src="img/images.jpg"  className="d-block" />
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src="img/lips.png"  className="d-block" />
                                     </div>
                                 </div>
-                                <div className="col-sm-8">
-                                    <p className="buy">
-                                        <button className="btn btn-buy">BUY</button>
-                                        <div className="nft-text">0.03 BNB per NFT</div>
-                                    </p>
-                                </div>
-
-
                             </div>
                         </div>
-
+                    </div>
+                    <div className="col-md-7">
+                        <p className="buy-container">
+                            <button className="btn btn-buy">BUY</button>
+                            <div className="nft-text">0.03 BNB per NFT</div>
+                        </p>
                     </div>
                     <div className="col-sm-2">
-                        <div className="d-flex justify-content-end">
+                        <div className="d-flex justify-content-center">
                             <div className="btn-group d-flex flex-column">
-                                <button className="btn btn-connect mt-5">CONNECT WALLET</button>
+                                <button className="btn btn-connect">CONNECT WALLET</button>
                                 <button className="btn btn-minting mt-5">MINTING</button>
                                 <button className="btn btn-play2E mt-5">PLAY 2E</button>
-                                <button className="btn btn-about mt-5">ABOUT</button>
+                                <button className="btn btn-about mt-5" onClick={about}>ABOUT</button>
                             </div>
                         </div>
                     </div>
