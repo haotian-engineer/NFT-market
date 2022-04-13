2906c394bad89a56562f6ac9dbd8484cd2016d9a
rabbit <rabbit@!.com>
2022-04-13 19:13:40 +0900

    fix

--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -1,28 +1,23 @@
 import React from "react";
 import './Home.css';
 
-import ConnectWalletButton from "./ConnectWalletButton";
-import Minting from "./Minting";
-import Play2E from "./Play2E";
-import About from "./About";
-
 export default function Home() {
     return (
         <div className="home">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-3">
-                        ff
-                    </div>
-                    <div className="col-sm-6">
-                        ddddd
-                    </div>
-                    <div className="col-sm-3">  
+            <div className="row">
+                <div className="col-sm-3">
+                    ff
+                </div>
+                <div className="col-sm-7">
+                    ddddd
+                </div>
+                <div className="col-sm-2">
+                    <div className="d-flex justify-content-center">
                         <div className="btn-group d-flex flex-column">
                             <button className="btn btn-connect mt-5">CONNECT WALLET</button>
-                            <button className="btn btn-connect mt-5">MINTING</button>
-                            <button className="btn btn-connect mt-5">PLAY 2E</button>
-                            <button className="btn btn-connect mt-5">ABOUT</button>
+                            <button className="btn btn-minting mt-5">MINTING</button>
+                            <button className="btn btn-play2E mt-5">PLAY 2E</button>
+                            <button className="btn btn-about mt-5">ABOUT</button>
                         </div>
                     </div>
                 </div>
